d3d33ab15e2644cf3989cf2c520067a35e684221
Julius Peter Ntale <juliusntale30@gmail.com>
2025-10-08 23:20:18 +0300

    feat(kafka): update Kafka broker images and configuration for improved stability

--- apps/client/src/components/ProductList.tsx
+++ apps/client/src/components/ProductList.tsx
@@ -174,7 +174,7 @@ const ProductList = async ({
       <Categories />
       {params === "products" && <Filter />}
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
-        {products.map((product) => (
+        {Array.isArray(products) &&products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
